cad89f554081111bf253f8fa48e7da6780c355fe
Your Name <you@example.com>
2019-03-14 12:11:34 +0330

    First Version

---
+++ src/main/java/ir/izolfaghari/constraintvalidation/ConstraintvalidationApplication.java
@@ -0,0 +1,13 @@
+package ir.izolfaghari.constraintvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstraintvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstraintvalidationApplication.class, args);
+	}
+
+}
